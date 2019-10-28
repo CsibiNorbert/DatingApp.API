a9f4e2638ecb2ce2edfd864c347e237cfded11c4
Norbert Csibi <cibi_norbert@yahoo.com>
2019-10-28 15:10:04 -0700

    Register user endpoint user Dto + validation

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Models;
+using DatingApp.API.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+
+        // Injecting the IAuthRepository to the controller.
+        public AuthController(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserToBeRegisteredDto userToBeRegistered)
+        {
+
+            userToBeRegistered.Username = userToBeRegistered.Username.ToLower();
+
+            if (await _authRepository.UserExists(userToBeRegistered.Username))
+            {
+                return BadRequest("Username already exists");
+            }
+
+            var userToCreate = new User
+            {
+                Username = userToBeRegistered.Username
+            };
+
+            var createdUder = await _authRepository.Register(userToCreate, userToBeRegistered.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
---
+++ Dtos/UserToBeRegisteredDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserToBeRegisteredDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(10,MinimumLength = 6,ErrorMessage = "Your password must be in between 6 and 10 characters")]
+        public string Password { get; set; }
+    }
+}
