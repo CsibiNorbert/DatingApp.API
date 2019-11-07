cf5d9b65cec76de692fd703736a77af4d96155fe
Norbert Csibi <cibi_norbert@yahoo.com>
2019-11-07 14:13:15 -0800

    Added updatedUserDto to map incoming data to the user object / Mapping for update user data / Create endpoint for update user data

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace DatingApp.API.Controllers
@@ -45,6 +46,32 @@ namespace DatingApp.API.Controllers
             var userToReturn = _mapper.Map<UserForDetailedDto>(user);
 
             return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+        {
+            // is matching the token for that particular user?
+            // Compare id of the path, with the user id from the token
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            // Retrieve user
+            var userFromRepo = await _datingrepo.GetUser(id);
+
+            // Map user with Dto
+            // It will take dto => user
+            _mapper.Map(userForUpdateDto,userFromRepo);
+
+            if (await _datingrepo.SaveAll())
+            {
+                return NoContent();
+            }
+
+            // If we reach here, something went wrong
+            throw new Exception($"Updating user with { id } failed on save");
         }
     }
 }
