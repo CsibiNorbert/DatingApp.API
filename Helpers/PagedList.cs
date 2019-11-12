5dc091d688347198564c1538d0c751b79dcdae11
Norbert Csibi <cibi_norbert@yahoo.com>
2019-11-12 14:33:07 -0800

    Created the helper class for the pagination functionality

---
+++ Helpers/PagedList.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Helpers
+{
+    // We might want to return more than one specific class, hence the T, generic
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            this.AddRange(items);
+        }
+
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber,int pageSize)
+        {
+            // total count of the items
+            var count = await source.CountAsync();
+
+            // bring our items, but we use the page numbers to workout what we send back
+            // (1-1) * 5 = 0 so we skip 0 and take from 5
+            // (2-1) * 5 = 5 skiped and take 5 more which is 10 etc etc.. ( PAGINATION CONCEPT )
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
