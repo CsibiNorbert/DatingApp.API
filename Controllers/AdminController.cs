76accf2645aa05ecc04373a27ea69594a8efbe24
Norbert Csibi <cibi_norbert@yahoo.com>
2020-02-15 14:09:03 -0800

    Edit roles from the admin side

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,94 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    /* 
+     * This class is to show how to implement policy based authorization.
+     * The Repository is not implemented for this class hence we have the DataContext in. 
+     */
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public AdminController(DataContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        [Authorize(Policy = "RequireAdminrole")]
+        [HttpGet("usersWithRoles")]
+        public async Task<IActionResult> GetUsersWithRoles()
+        {
+            // Order by name and then we are going to project to a new object (we use select)
+            // Construct a new object
+            // Find out which roles the user belongs to and return the role name. We use not linq query but query expresion instead
+            var userList = await _context.Users
+                .OrderBy(x => x.UserName)
+                .Select(u => new
+                {
+                    Id = u.Id,
+                    UserName = u.UserName,
+                    Roles = (from userRole in u.UserRoles
+                             join role in _context.Roles on userRole.RoleId
+                             equals role.Id
+                             select role.Name).ToList()
+                }).ToListAsync();
+            return Ok(userList);
+        }
+        /*
+         Edit the roles for a user
+         */
+        [Authorize(Policy = "RequireAdminrole")]
+        [HttpPost("editRoles/{userName}")]
+        public async Task<IActionResult> EditRoles(string userName, RoleEditDto roleEditDto)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+
+            // Get roles for this particullar user
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            // This roles are selected on the frontend
+            var selectedRoles = roleEditDto.RoleNames;
+
+            // taking in consideration that the user has been de-selected from all the roles.
+            // If roles are empty create an empty object
+            selectedRoles = selectedRoles ?? new string[] { };
+
+            var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if (!result.Succeeded)
+            {
+                return BadRequest("Failed to add to roles");
+            }
+            
+            result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded)
+            {
+                return BadRequest("Failed to remove the roles");
+            }
+
+            return Ok(await _userManager.GetRolesAsync(user));
+        }
+
+        [Authorize(Policy = "RequireModeratorRole")]
+        [HttpGet("photosForModeration")]
+        public IActionResult GetPhotosForModeration()
+        {
+            return Ok("Admins or moderators can see this");
+        }
+    }
+}
